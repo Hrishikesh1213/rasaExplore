ff8b3e42cb7145eb0f874e95a1574fddc9f1cc07
Hrishikesh Karra <khr10@iitbbs.ac.in>
2021-08-28 16:11:11 +0530

    Connected twilio(whatsApp-> rasa -> Node-Red server)

--- trial2/actions/actions.py
+++ trial2/actions/actions.py
@@ -13,28 +13,38 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+from twilio.rest import Client 
+
 class ActionHelloWorld(Action):
 
     def name(self) -> Text:
-        return "action_make_the_post"
+        return "action_post_data"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # x = requests.get('https://jsonplaceholder.typicode.com/posts/1').json()
 
         cuisine = tracker.get_slot('cuisine')
         num_people = tracker.get_slot('num_people')
+        # slot_number = tracker.get_slot('slot_number')
+
+        # account_sid = "AC90e2d0be773c5e1eb80153cfe65b7221"
+        # auth_token =  "3af6d18255b49d61a6a83a070430681f"
+        # client = Client(account_sid, auth_token) 
+
+        # sender = 'whatsapp:+14155238886'
+        # reciever = 'whatsapp:+919381567454'
+
+        # URL = "https://jsonplaceholder.typicode.com/posts/{0}".format(slot_number)
 
         dataobj = {
             'cuisine': cuisine,
             'num_people': num_people
         }
         z = requests.post('http://127.0.0.1:1880/server1', data = dataobj)
-        # if z.status_code == 200:
-        #     y = 'successfully posted'
-        # else:
-        #     y = json.dumps(x)
+        # x = requests.get(URL).json()
+        # y = json.dumps(x)
+        
         dispatcher.utter_message(text=str(z.status_code))
 
         return []
